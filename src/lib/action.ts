d71043057ff2062d05cf0229f92efe666263318b
AminaSheika <amina@hashinclu.de>
2024-12-06 21:54:33 +0400

    changes

--- src/lib/action.ts
+++ src/lib/action.ts
@@ -1,14 +1,19 @@
 export async function getLoggedInUser() {
     try {
-      const accessToken = localStorage.getItem("access_token");
-      if (accessToken){
-        return accessToken;
-      }
-      else{
+      if (typeof window !== "undefined" && window.localStorage) {
+        const accessToken = localStorage.getItem("access_token");
+        if (accessToken) {
+          return accessToken;
+        } else {
+          return null;
+        }
+      } else {
+        // If localStorage is not available, return null or handle accordingly
         return null;
       }
     } catch (error) {
-      console.log(error)
+      console.error(error);
       return null;
     }
   }
+  
