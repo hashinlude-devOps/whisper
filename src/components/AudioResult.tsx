8f5adacc34e1db13409a7a789d747d4e1803b410
Abhijith K <abhijith@hashinclu.de>
2024-12-09 02:05:46 +0400

    Fix : REsolved conflict

--- src/components/AudioResult.tsx
+++ src/components/AudioResult.tsx
@@ -172,28 +172,16 @@ const AudioResultComponent = ({ id }: { id: number }) => {
             </div>
           </div>
 
-          <div className="flex flex-row justify-between">
-            <div className="flex items-center space-x-2">
-              <div className="flex items-center space-x-1">
-                <CalendarIcon className="h-5 w-5 text-gray-500" />
-                <span className="text-sm text-gray-700">
-                  27/11/2024, 15:32:38
-                </span>
-              </div>
-              <div className="flex items-center space-x-1">
-                <ClockIcon className="h-5 w-5 text-gray-500" />
-                <span className="text-sm text-gray-700">0:26</span>
-              </div>
+          <div className="flex items-center space-x-2">
+            <div className="flex items-center space-x-1">
+              <CalendarIcon className="h-5 w-5 text-gray-500" />
+              <span className="text-sm text-gray-700">
+                27/11/2024, 15:32:38
+              </span>
             </div>
-
-            <div className="cursor-pointer">
-              <Button
-                type="primary"
-                className="bg-gray-800 w-full sm:w-auto sm:self-center"
-                onClick={() => router.push(`minutes/${id}`)}
-              >
-                Meeting minutes
-              </Button>
+            <div className="flex items-center space-x-1">
+              <ClockIcon className="h-5 w-5 text-gray-500" />
+              <span className="text-sm text-gray-700">0:26</span>
             </div>
           </div>
         </div>
